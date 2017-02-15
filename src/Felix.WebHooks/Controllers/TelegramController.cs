d06618f7a2e9bdbe7fbde8809bc3109cf1f3fc65
Timur Smirnov <tsmirnov@live.ru>
2017-02-16 02:07:46 +0300

    added controller for hooks and some libs

---
+++ src/Felix.WebHooks/Controllers/TelegramController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Telegram.Bot.Types;
+
+namespace Felix.WebHooks.Controllers
+{
+    [RoutePrefix("api/telegram")]
+    public class TelegramController : ApiController
+    {
+        private ILogger _logger;
+
+
+        public TelegramController()
+        {
+
+        }
+
+        public TelegramController(ILogger logger)
+        {
+            _logger = logger;        
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public async Task<IHttpActionResult> Post(Update update)
+        {
+            _logger?.WriteInformation(JsonConvert.SerializeObject(update));
+            return await Task.FromResult(Ok("I got message"));
+        }
+
+        
+    }
+}
---
+++ src/Felix.WebHooks/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Cors;
+using Microsoft.Owin.Logging;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+using Telegram.Bot.Types;
+
+namespace Felix.WebHooks
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+            
+            app.UseWebApi(config);
+            app.UseCors(CorsOptions.AllowAll);
+
+            var container = new UnityContainer();
+
+            var logger = new Microsoft.Owin.Logging.DiagnosticsLoggerFactory().Create("Default");
+
+            container.RegisterInstance<ILogger>(logger);
+            
+            var update = new Update();
+            var json = JsonConvert.SerializeObject(update);
+
+        }
+    }
+}
